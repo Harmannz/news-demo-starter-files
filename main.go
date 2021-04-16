0ae69be855c3305a3d29449dcc397717657376a2
harmannz <harman.singh@hotmail.co.nz>
2021-04-17 02:05:25 +1200

    Load static Demo page

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
+package main
 
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var pageTemplate = template.Must(template.ParseFiles("index.html"))
+
+func indexHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	pageTemplate.Execute(responseWriter, nil)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
