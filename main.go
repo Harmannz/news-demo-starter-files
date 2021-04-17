e0371a3bad166a5e10d69db11f8c07c24aa9aea4
harmannz <harman.singh@hotmail.co.nz>
2021-04-17 22:30:31 +1200

    Create API client

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"github.com/harmannz/news-demo-starter-files/news"
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -13,6 +17,26 @@ var pageTemplate = template.Must(template.ParseFiles("index.html"))
 
 func indexHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	pageTemplate.Execute(responseWriter, nil)
+}
+
+func searchHandler(newsapi *news.Client) http.HandlerFunc {
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		parsedURL, err := url.Parse(request.URL.String())
+		if err != nil {
+			http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		params := parsedURL.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+
+		fmt.Println("Search Query is: ", searchQuery)
+		fmt.Println("Page is: ", page)
+	}
 }
 
 func main() {
@@ -26,8 +50,17 @@ func main() {
 		port = "3000"
 	}
 
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Env: apiKey must be set")
+	}
+
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsAPI := news.NewClient(myClient, apiKey, 20)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/search", searchHandler(newsAPI))
 
 	fs := http.FileServer(http.Dir("assets"))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
